8b01e82e59c520ef9e4802123f3788a4b8a4ad72
Alain Kaeslin <alain@cloudscale.ch>
2025-12-19 11:15:31 +0100

    Use `topologyZonePrefix` for zone key in accessibility requirements parsing.

--- driver/controller.go
+++ driver/controller.go
@@ -87,7 +87,7 @@ func (d *Driver) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest)
 
 	if req.AccessibilityRequirements != nil {
 		for _, t := range req.AccessibilityRequirements.Requisite {
-			zone, ok := t.Segments["zone"]
+			zone, ok := t.Segments[topologyZonePrefix]
 			if !ok {
 				continue // nothing to do
 			}
